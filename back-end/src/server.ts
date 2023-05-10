a2524e3eaea2082a5ea3cff9b6ce34231c536ad9
Your Name <lucasdosualdo@gmail.com>
2023-05-10 06:06:22 -0300

    fix: update preview images

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
